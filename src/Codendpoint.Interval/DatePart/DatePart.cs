958e72124a18a7f2cb886e693e57ce4f3b389c79
Alejandro Rodriguez Suarez <arsmbms@hotmail.com>
2016-09-09 20:31:11 -0400

    in a middle of the implementation

---
+++ src/Codendpoint.Interval/DatePart/DatePart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codendpoint.Interval.DatePart
+{
+    public abstract class DatePart
+    {
+        public abstract string Identifier { get; }
+
+        public abstract int GetValue(DateTime date);
+
+        public abstract int GetDomain(DateTime date);
+
+        public bool Evaluate(DateTime date, int exact, int frequency, bool reverse)
+        {
+            var value = GetValue(date);
+            if (reverse)
+                value = GetDomain(date) - value + 1;
+
+            if (frequency == 0)
+                return value == exact;
+
+            return (value - exact) / frequency > 0 && (value - exact) % frequency == 0;
+        }
+    }
+}
---
+++ src/Codendpoint.Interval/DatePart/DayInMonth.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codendpoint.Interval.DatePart
+{
+    public class DayInMonth : DatePart
+    {
+        public override string Identifier
+            => "d";
+
+        public override int GetDomain(DateTime date)
+        {
+            switch (date.Month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    return 31;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return 30;
+                case 2:
+                    return year % 4 == 0 ? 
+
+            }
+        }
+
+        public override int GetValue(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
